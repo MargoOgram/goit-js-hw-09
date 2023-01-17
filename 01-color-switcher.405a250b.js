1e187e147c327c5340977509ac02ee870bee10b9
MargoOgram <109178025+MargoOgram@users.noreply.github.com>
2023-01-17 18:58:39 +0000

    Deploying to gh-pages from @ MargoOgram/goit-js-hw-09@64cc095b725d4febe9c4914c8ab9a2d177ac4d8b 🚀

---
+++ 01-color-switcher.405a250b.js
@@ -0,0 +1,2 @@
+const t=document.querySelector("[data-start]"),e=document.querySelector("[data-stop]"),o=document.querySelector("body");let n=null;function r(){let t=`#${Math.floor(16777215*Math.random()).toString(16)}`;o.style.backgroundColor=t}t.addEventListener("click",(function(){n=setInterval(r,1e3),t.toggleAttribute("disabled")})),e.addEventListener("click",(function(){clearInterval(n),t.removeAttribute("disabled")}));
+//# sourceMappingURL=01-color-switcher.405a250b.js.map
---
+++ 01-color-switcher.524fd769.js
@@ -0,0 +1,2 @@
+!function(){var t=document.querySelector("[data-start]"),e=document.querySelector("[data-stop]"),n=document.querySelector("body"),o=null;function r(){var t="#".concat(Math.floor(16777215*Math.random()).toString(16));n.style.backgroundColor=t}t.addEventListener("click",(function(){o=setInterval(r,1e3),t.toggleAttribute("disabled")})),e.addEventListener("click",(function(){clearInterval(o),t.removeAttribute("disabled")}))}();
+//# sourceMappingURL=01-color-switcher.524fd769.js.map
